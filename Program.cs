1a47e3c2362c25437bc6bb7d090ee56ca295687c
dasha <dasha15042009@yandex.ru>
2022-09-29 21:24:51 +0300

    check first step

--- Program.cs
+++ Program.cs
@@ -1,5 +1,12 @@
-﻿void FillArray(string[] arr)
+﻿string[] FillArray(int n)
 {
-string[] array = {"1234", "1567", "-2", "computer science"};
+    string[] array = new string[4] {"1234", "1567", "-2", "computer science"};
+    return array;
+}
 
+
+string[] array = FillArray(4);
+for(int i=0; i<array.Length;i++)
+{
+    Console.WriteLine(array[i]);
 }
