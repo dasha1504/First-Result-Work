dc95d897ace0cb3fadc909903fe4021d0e574733
dasha <dasha15042009@yandex.ru>
2022-09-29 21:32:20 +0300

    new fillarray

--- Program.cs
+++ Program.cs
@@ -10,3 +10,16 @@ for(int i=0; i<array.Length;i++)
 {
     Console.WriteLine(array[i]);
 }
+string[] FillArray2(string[] array)
+{
+    string[] array2 = new string[4];
+    for(int i=0; i<array2.Length; i++)
+    {
+        if(array2[i].Length <= 3)
+        {
+            array2[i] = array[i];
+        }
+
+    }
+}
+string[] result = FillArray2(array);
